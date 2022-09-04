66a5e7ec3f5958d55bd68020b27f796da137f21e
Ayaan <kaifullah7921@gmail.com>
2022-09-04 17:13:22 +0530

    initial build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {  Amplify } from "aws-amplify";
-import { withAuthenticator, Button, Heading, AmplifySignOut } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import {
+  withAuthenticator,
+  AmplifySignOut,
+  Heading,
+} from "@aws-amplify/ui-react";
+// import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
 
 Amplify.configure(awsExports);
@@ -11,9 +15,8 @@ function App({ signOut, user }) {
   return (
     <div className="App">
       <header className="App-header">
-        <Heading level={1}>Hello {user.username}</Heading>
-        {/* <Button onClick={signOut}>Sign out</Button> */}
-      <AmplifySignOut/>
+        Hello {user.username}
+        <AmplifySignOut />
       </header>
     </div>
   );
